72381fdc347383349e66a62c4a385a0caa6dea63
Faheem Saleem <63436748+PinchedRainbow@users.noreply.github.com>
2023-05-27 02:00:24 +0500

    Got cookies & access tokens!!!!1

--- app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
+++ app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
@@ -1,13 +1,16 @@
 package com.jawaadianinc.valorant_stats.valo.activities.new_ui
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import android.widget.Toast
 import com.jawaadianinc.valorant_stats.R
 import com.jawaadianinc.valorant_stats.databinding.ActivityNewLogInUiBinding
@@ -15,6 +18,8 @@ import com.jawaadianinc.valorant_stats.databinding.ActivityNewLogInUiBinding
 class NewLogInUI : AppCompatActivity() {
     lateinit var binding : ActivityNewLogInUiBinding
     lateinit var webView : WebView
+    lateinit var valoLogo: ImageView
+    lateinit var staticsLogo: ImageView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,32 +28,37 @@ class NewLogInUI : AppCompatActivity() {
         setContentView(binding.root)
 
         webView = binding.riotSignInWebVIEW
+        valoLogo = binding.imageView11
+        staticsLogo = binding.imageView10
 
         webView.settings.javaScriptEnabled = true
         //val url = "https://auth.riotgames.com/authorize?client_id=statics&redirect_uri=https://statics-fd699.web.app/authorize.html&response_type=code&scope=openid+offline_access&prompt=login"
-        val url = "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&prompt=login"
+        val RiotURL = "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&prompt=login"
         // track cookies
         CookieManager.getInstance().setAcceptCookie(true)
-        webView.loadUrl(url)
-
-        // get the cookie for the specified domain
-        val cookie: String = CookieManager.getInstance().getCookie(url) ?: ""
-        // toast the access token
-        Toast.makeText(this@NewLogInUI, "Cookie: $cookie", Toast.LENGTH_SHORT).show()
+        webView.loadUrl(RiotURL)
 
         webView.webViewClient = object : WebViewClient() {
             @Deprecated("Deprecated in Java")
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 // check if the url contains the code
                 if (url.contains("access_token=")) {
-                    // get the cookie for the specified domain "auth.riotgames.com"
-                    val cookieManager = CookieManager.getInstance()
-                    val domain = "auth.riotgames.com" // Set the correct domain
-                    val cookie = cookieManager.getCookie(domain)
-                    Toast.makeText(this@NewLogInUI, "Cookie: $cookie", Toast.LENGTH_SHORT).show()
+                    // get the cookies from the webview
+                    val cookies = CookieManager.getInstance().getCookie(RiotURL)
+                    val accessToken = url.split("access_token=")[1].split("&")[0]
                 }
-                return false
+                // else if the url doesn't contain the code, load the url
+                else {
+                    view.loadUrl(url)
+                }
+                return true
             }
+        }
+
+        fun authoriseUser(accessToken: String, cookies: String)
+        {
+            // hide the webview
+            webView.alpha = 0f
         }
     }
 }
