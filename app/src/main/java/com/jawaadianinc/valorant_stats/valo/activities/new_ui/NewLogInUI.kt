eb534bd41c5d9a4c696f33da5f0cea871021afcf
Faheem Saleem <63436748+PinchedRainbow@users.noreply.github.com>
2023-05-27 01:33:35 +0500

    Correct URL Auth :D

--- app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
+++ app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
@@ -1,6 +1,8 @@
 package com.jawaadianinc.valorant_stats.valo.activities.new_ui
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.CookieManager
@@ -23,21 +25,27 @@ class NewLogInUI : AppCompatActivity() {
         webView = binding.riotSignInWebVIEW
 
         webView.settings.javaScriptEnabled = true
-        val url = "https://auth.riotgames.com/authorize?client_id=statics&redirect_uri=https://statics-fd699.web.app/authorize.html&response_type=code&scope=openid+offline_access&prompt=login"
+        //val url = "https://auth.riotgames.com/authorize?client_id=statics&redirect_uri=https://statics-fd699.web.app/authorize.html&response_type=code&scope=openid+offline_access&prompt=login"
+        val url = "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&prompt=login"
+        // track cookies
+        CookieManager.getInstance().setAcceptCookie(true)
         webView.loadUrl(url)
 
-        // hear the url change
+        // get the cookie for the specified domain
+        val cookie: String = CookieManager.getInstance().getCookie(url) ?: ""
+        // toast the access token
+        Toast.makeText(this@NewLogInUI, "Cookie: $cookie", Toast.LENGTH_SHORT).show()
+
         webView.webViewClient = object : WebViewClient() {
             @Deprecated("Deprecated in Java")
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 // check if the url contains the code
-                if (url.contains("code=")) {
-                    // get the cookie from the url
-                    val cookie = CookieManager.getInstance().getCookie(url)
-                    // get the ssid from the cookie
-                    val ssid = cookie.split(" ")[0].split("=")[1]
-                    Toast.makeText(this@NewLogInUI, "Found Cookie: $ssid", Toast.LENGTH_SHORT).show()
-
+                if (url.contains("access_token=")) {
+                    // get the cookie for the specified domain "auth.riotgames.com"
+                    val cookieManager = CookieManager.getInstance()
+                    val domain = "auth.riotgames.com" // Set the correct domain
+                    val cookie = cookieManager.getCookie(domain)
+                    Toast.makeText(this@NewLogInUI, "Cookie: $cookie", Toast.LENGTH_SHORT).show()
                 }
                 return false
             }
