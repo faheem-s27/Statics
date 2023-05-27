b8fd3e1b998bdc02d1cac7522739d43422cd1df2
Faheem Saleem <63436748+PinchedRainbow@users.noreply.github.com>
2023-05-27 14:55:14 +0500

    Sigh

--- app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
+++ app/src/main/java/com/jawaadianinc/valorant_stats/valo/activities/new_ui/NewLogInUI.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.CookieManager
 import android.webkit.WebView
@@ -29,6 +30,7 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.net.URL
+import javax.security.auth.Subject
 
 class NewLogInUI : AppCompatActivity() {
     lateinit var binding : ActivityNewLogInUiBinding
@@ -49,7 +51,7 @@ class NewLogInUI : AppCompatActivity() {
 
         webView.settings.javaScriptEnabled = true
         //val url = "https://auth.riotgames.com/authorize?client_id=statics&redirect_uri=https://statics-fd699.web.app/authorize.html&response_type=code&scope=openid+offline_access&prompt=login"
-        val RiotURL = "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&prompt=login"
+        val RiotURL = "https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1"
         // track cookies
         CookieManager.getInstance().setAcceptCookie(true)
         webView.loadUrl(RiotURL)
@@ -72,7 +74,6 @@ class NewLogInUI : AppCompatActivity() {
                     val cookies = CookieManager.getInstance().getCookie(RiotURL)
                     val accessToken = url.split("access_token=")[1].split("&")[0]
                     authoriseUser(accessToken, cookies)
-
                 }
                 // else if the url doesn't contain the code, load the url
                 else {
@@ -86,9 +87,9 @@ class NewLogInUI : AppCompatActivity() {
     private fun addStringToTextView(text: String)
     {
         // animate the textview
-        updateText.animate().alpha(0f).setDuration(500).withEndAction {
+        updateText.animate().alpha(0f).setDuration(100).withEndAction {
             updateText.text = updateText.text.toString() + "\n" + text
-            updateText.animate().alpha(1f).setDuration(500).start()
+            updateText.animate().alpha(1f).setDuration(100).start()
         }.start()
     }
 
@@ -98,8 +99,12 @@ class NewLogInUI : AppCompatActivity() {
         webView.alpha = 0f
         // make a loading progress dialog in the middle of the screen
         GlobalScope.launch {
-            withContext(Dispatchers.Main) {
+            withContext(Dispatchers.Main)
+            {
                 addStringToTextView("Got cookies 🍪")
+            }
+            delay(200)
+            withContext(Dispatchers.Main) {
                 addStringToTextView("Got access token 🎫")
             }
             val RiotVersion = getRiotClientVersion()
@@ -117,7 +122,7 @@ class NewLogInUI : AppCompatActivity() {
                     addStringToTextView("Got entitlement 📜")
                 }
             }
-            val userInfo = getUserInfo(accessToken, RiotVersion.first, RiotVersion.second)
+            val userInfo = getUserInfo(accessToken, RiotVersion.first, RiotVersion.second, entitlement, cookies)
             withContext(Dispatchers.Main) {
                 if (userInfo == "Error")
                 {
@@ -128,10 +133,12 @@ class NewLogInUI : AppCompatActivity() {
                     addStringToTextView("Got user info 👤")
                 }
             }
+
+            val partyTest = getPartyTest(accessToken, entitlement, userInfo, RiotVersion.first, RiotVersion.second)
         }
     }
 
-    private fun getUserInfo(accessToken: String, build:String, clientVersion:String): String
+    private fun getUserInfo(accessToken: String, build:String, clientVersion:String, entitlement: String, cookies: String): String
     {
         return runBlocking(Dispatchers.IO) {
             val client = OkHttpClient()
@@ -140,11 +147,14 @@ class NewLogInUI : AppCompatActivity() {
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Authorization", "Bearer $accessToken")
                 .addHeader("X-Riot-ClientVersion", build)
+                .addHeader("X-Riot-Entitlements-JWT", entitlement)
                 .addHeader("X-Riot-ClientPlatform", clientPlatformToken)
+                .addHeader("Cookie", cookies)
                 .addHeader(
                     "User-Agent",
                     "RiotClient/$clientVersion rso-auth (Windows; 10;;Professional, x64)"
                 )
+                .get()
                 .build()
             val userInfoResponse = client.newCall(userInfoRequest).execute()
             val code = userInfoResponse.code
@@ -155,16 +165,17 @@ class NewLogInUI : AppCompatActivity() {
                 return@runBlocking "Error"
             }
             val json = JSONObject(body)
-            val sub = json.getString("sub")
+            Log.d("RSO", "User info: $body")
+            json.getString("sub")
 
             // check if json has object "acct"
-            if (!json.has("acct"))
-            {
-                return@runBlocking "Error"
-            }
-            val gameName = json.getJSONObject("acct").getString("game_name")
-            val tagLine = json.getJSONObject("acct").getString("tag_line")
-            return@runBlocking "$sub:$gameName:$tagLine"
+//            if (!json.has("acct"))
+//            {
+//                return@runBlocking "Error"
+//            }
+//            val gameName = json.getJSONObject("acct").getString("game_name")
+//            val tagLine = json.getJSONObject("acct").getString("tag_line")
+//            return@runBlocking "$sub:$gameName:$tagLine"
         }
     }
 
@@ -200,12 +211,46 @@ class NewLogInUI : AppCompatActivity() {
                 .build()
             val response = client.newCall(request).execute()
             val code = response.code
-            val body = response.body
+            val body = response.body.string()
             if (code != 200) {
                 return@runBlocking "Error"
             }
-            val JSON = JSONObject(body.string())
-            JSON.getString("entitlements_token")
+
+            Log.d("RSO", "Entitlements: $body")
+
+            JSONObject(body).getString("entitlements_token")
         }
     }
+
+    private fun getPartyTest(accessToken: String, entitlement: String, subject: String, build: String, clientVersion: String) : String
+    {
+        val client = OkHttpClient()
+        val url = "https://pd.eu.a.pvp.net/personalization/v2/players/${subject}/playerloadout"
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("X-Riot-Entitlements-JWT", entitlement)
+            .addHeader("X-Riot-ClientPlatform", clientPlatformToken)
+            .addHeader("X-Riot-ClientVersion", build)
+            .addHeader("Authorization", "Bearer $accessToken")
+            .addHeader(
+                "User-Agent",
+                "RiotClient/$clientVersion rso-auth (Windows; 10;;Professional, x64)"
+            )
+            .get()
+            .build()
+
+        return runBlocking(Dispatchers.IO) {
+            val response = client.newCall(request).execute()
+            val code = response.code
+            val body = response.body.string()
+            Log.d("RSO", "Party test: $body")
+            if (code != 200) {
+                return@runBlocking "Error"
+            }
+            return@runBlocking body
+        }
+
+    }
+
 }
